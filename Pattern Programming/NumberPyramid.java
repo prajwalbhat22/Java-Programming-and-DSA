e454c78f2b88897887896761ba6b73de1e18d030
prajwal <prajwalbhat220@gmail.com>
2026-06-24 23:17:10 +0530

    pattern problems

---
+++ Pattern Programming/NumberPyramid.java
@@ -0,0 +1,20 @@
+public class NumberPyramid {
+    public static void main(String[] args) {
+
+        int rows = 5;
+
+        for (int i = 1; i <= rows; i++) {
+
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+
+            System.out.println();
+        }
+    }
+    
+}
