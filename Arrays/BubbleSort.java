c70366a94ac6e7e9692a5fa837ae1c693128fd21
prajwal <prajwalbhat220@gmail.com>
2026-06-29 22:58:39 +0530

    Add Binary Search and Bubble Sort array  programs

---
+++ Arrays/BubbleSort.java
@@ -0,0 +1,28 @@
+package Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+
+        int[] arr = {40, 10, 50, 20, 30};
+
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                }
+            }
+        }
+
+        System.out.println("Sorted Array:");
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
